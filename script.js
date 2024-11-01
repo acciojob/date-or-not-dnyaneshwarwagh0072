429972e23f98aa1bcde7fe6b853c411a5781343c
Dnyaneshwar Yadav Wagh <85550894+dnyaneshwarwagh0072@users.noreply.github.com>
2024-11-01 18:26:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 var isDate = function (input) {
-  //   write your code here
+    if (input instanceof Date && !isNaN(input)) {
+        return true;
+    }
+    
+    
+    const date = new Date(input);
+    return !isNaN(date.getTime());
 };
 
 // Do not change the code below.
