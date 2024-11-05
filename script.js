cf73322d2e972f01266ca2a63126b9c2aba49690
Dnyaneshwar Yadav Wagh <85550894+dnyaneshwarwagh0072@users.noreply.github.com>
2024-11-05 13:32:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 var isDate = function (input) {
-    if (input instanceof Date && !isNaN(input)) {
-        return true;
-    }
-    if (typeof input === 'string' || typeof input === 'number') {
-        const parsedDate = new Date(input);
-        return !isNaN(parsedDate);
-    }
-    return false;
+        return !isNaN(Date.parse(input));
 };
-
-// Do not change the code below.
 const input = prompt("Enter Date.");
 alert(isDate(input));
